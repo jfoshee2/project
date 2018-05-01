04060963156685911807ef0833cbb66506b79356
jfoshee2 <jfoshee2@students.kennesaw.edu>
2018-05-01 17:22:13 -0400

    First Commit

---
+++ leads/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+from leads.models import Lead
+from leads.serializers import LeadSerializer
+
+
+class LeadListCreate(generics.ListCreateAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
